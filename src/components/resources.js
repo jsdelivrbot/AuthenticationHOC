279e69f0398cbc036152e3e368b47febcf2dec70
Nicole Pellicena <nicole.pellicena@unrulygroup.com>
2017-05-04 11:45:53 +0100

    Add resources component and react-router

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <div>
+            <h4>Super Secret Resources</h4>
+            <ul>
+                <li>Crack Jennys tea cup quarterdeck killick blow</li>
+                <li>The man down clap of thunder six pounders fire in the hole ahoy</li>
+                <li>Jack coxswain Chain Shot boom lee barkadeer list strike colors</li>
+            </ul>
+        </div>
+    )
+}
